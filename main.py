7b93bf6f1c27edeb9107b8845fef6a581afde4b3
dapp.1990 <dapp.199@gmail.com>
2017-05-15 19:37:28 +0200

    version main_2_1

--- main.py
+++ main.py
@@ -10,12 +10,15 @@ from ryu.lib import hub
 import htb
 
 # Global variables
-queue_frames = []  # In theory lists are thread-safe
+queue_uplink = []  # In theory lists are thread-safe
+queue_downlink = []  # In theory lists are thread-safe
 # rate NOT: 2kbps because it takes 2000000 cycles to storage MTU
 # so a single packet with 1556 is delayed ~ 2 seconds
 
-rate = 50000  # bytes/second
-capacity = 1  # 32 -> 49792 -> roughly 50000
+rate = 300000  # bytes/second
+# 32 -> 49792 -> roughly 50000
+# 1 -> busrt == MTU -> no burst
+capacity = 66
 
 
 class QoSManager(app_manager.RyuApp):
@@ -23,7 +26,8 @@ class QoSManager(app_manager.RyuApp):
 
     def __init__(self, *args, **kwargs):
         super(QoSManager, self).__init__(*args, **kwargs)
-        self.scheduler_thread = hub.spawn(self._TBF_scheduler)
+        self.uplink_thread = hub.spawn(self._TBF_scheduler_uplink)
+        self.downlink_thread = hub.spawn(self._TBF_scheduler_downlink)
 
     @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
     def switch_features_handler(self, ev):
@@ -82,7 +86,6 @@ class QoSManager(app_manager.RyuApp):
 
         if in_port == 1:
             # Send out to local port which is connected with the Linux Kernel
-            # out_port = 4294967294L
             out_port = 4294967294
         else:
             out_port = 1
@@ -92,20 +95,19 @@ class QoSManager(app_manager.RyuApp):
             self.logger.info("A valid buffer was found!: %s", msg.buffer_id)
             # return
 
-        # Create the out package format to be sent
+        # Create the out paket format to be sent
         data = msg.data  # problably not useful
         actions = [parser.OFPActionOutput(out_port)]
         out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id, in_port=in_port, actions=actions,
                                   data=data)
 
         # Todo: Assign QoS to datapath
-        """
         if out_port == 4294967294:
-            datapath.send_msg(out)
+            queue_downlink.append((ev.msg.msg_len, datapath, out))
         else:
-            queue_frames.append((ev.msg.msg_len, datapath, out)) #only assign QoS to downlink transmition
-        """
-        queue_frames.append((ev.msg.msg_len, datapath, out))
+            queue_uplink.append((ev.msg.msg_len, datapath, out))
+
+            # todo: clear a class of scheduling methods, pass reference of this class to send back the packets
 
     def _fifo_scheduler(self):
         while True:
@@ -115,7 +117,9 @@ class QoSManager(app_manager.RyuApp):
             # Delay
             hub.sleep(0.001)
 
-    def _TBF_scheduler(self):
+    # todo: clean duplicate code
+
+    def _TBF_scheduler_uplink(self):
         # The limit is given by queue_frames,
         tbf = htb.HTB([capacity], [rate], 1)
 
@@ -126,9 +130,9 @@ class QoSManager(app_manager.RyuApp):
 
         self.logger.info("TBF scheduler: %s", msg_len)
         while True:
-            if queue_frames:
+            if queue_uplink:
                 if consume_package:
-                    msg_len, datapath, out_format = queue_frames.pop()
+                    msg_len, datapath, out_format = queue_uplink.pop()
                     consume_package = False
 
             if not consume_package:
@@ -136,3 +140,26 @@ class QoSManager(app_manager.RyuApp):
                     datapath.send_msg(out_format)
                     consume_package = True
             hub.sleep(0.001)  # a sleep greater than 0.001 does not work properly... a big delay is encountered
+
+    def _TBF_scheduler_downlink(self):
+        # The limit is given by queue_frames,
+        tbf = htb.HTB([capacity], [rate], 1)
+
+        msg_len = 0
+        datapath = None
+        out_format = None
+        consume_package = True
+
+        self.logger.info("TBF scheduler: %s", msg_len)
+        while True:
+            if queue_downlink:
+                if consume_package:
+                    msg_len, datapath, out_format = queue_downlink.pop()
+                    consume_package = False
+
+            if not consume_package:
+                if tbf.consume(0, msg_len):
+                    datapath.send_msg(out_format)
+                    consume_package = True
+            hub.sleep(0.001)  # a sleep greater than 0.001 does not work properly... a big delay is encountered
+
