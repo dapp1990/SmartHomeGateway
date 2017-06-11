3f41558393e2b150cb14badef70d30fef1f34948
Daniel Andrés Pérez Pérez <dapp.1990@gmail.com>
2017-06-11 15:19:50 +0200

    Revome serialization to avoid error serialization on API

--- statistics_module/simple_statistics_manager.py
+++ statistics_module/simple_statistics_manager.py
@@ -1,21 +1,7 @@
 from .interface_manager import InterfaceStatistics
 from tinydb import TinyDB
-from tinydb_serialization import SerializationMiddleware
 from tinydb import Query
 from datetime import datetime
-from tinydb_serialization import Serializer
-import time
-
-
-# https://github.com/msiemens/tinydb-serialization
-class DateTimeSerializer(Serializer):
-    OBJ_CLASS = datetime  # The class this serializer handles
-
-    def encode(self, obj):
-        return obj.strftime('%Y-%m-%dT%H:%M:%S')
-
-    def decode(self, s):
-        return datetime.strptime(s, '%Y-%m-%dT%H:%M:%S')
 
 
 class SimpleStatisticsManager(InterfaceStatistics):
@@ -23,24 +9,28 @@ class SimpleStatisticsManager(InterfaceStatistics):
     """
 
     def __init__(self, database_name):
-        self.serialization = SerializationMiddleware()
-        self.serialization.register_serializer(DateTimeSerializer(), 'TinyDate')
-        self.db = TinyDB('{}'.format(database_name),storage=self.serialization)
+        self.db_name = database_name
 
     def save_statistics(self, statistics):
+        db = TinyDB('{}'.format(self.db_name))
         flow_id = statistics[0] + statistics[1]
-        self.db.insert({'flow_id': flow_id, 'size': statistics[2],
-                        'time': datetime.strptime(statistics[3], '%Y-%m-%d '
-                                                                 '%H:%M:%S.%f')})
+        db.insert({'flow_id': flow_id, 'size': statistics[2], 'time':
+                   statistics[3]})
+        db.close()
         return True
 
     def get_statistics(self, flow_id, max_stat, time_from_str, time_to_str):
+        db = TinyDB('{}'.format(self.db_name))
+        query = db.search(Query().flow_id == flow_id)
+        db.close()
         time_from = datetime.strptime(time_from_str, '%Y-%m-%d %H:%M:%S.%f')
         time_to = datetime.strptime(time_to_str, '%Y-%m-%d %H:%M:%S.%f')
-        query = self.db.search(Query().flow_id == flow_id)
+
         result = []
         for dic in query:
-            if time_from < dic['time'] < time_to:
+            correct_format = datetime.strptime(dic['time'],
+                                               '%Y-%m-%d %H:%M:%S.%f')
+            if time_from < correct_format < time_to:
                 result.append(dic['size'])
         size = len(result)
         if size > max_stat:
