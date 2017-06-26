8102dbcbeae031758af375b7635e3f010168db13
Daniel Andrés Pérez Pérez <dapp.1990@gmail.com>
2017-06-26 14:56:55 +0200

    Revert SimpleStatistics object

--- statistics_module/simple_statistics_manager.py
+++ statistics_module/simple_statistics_manager.py
@@ -12,15 +12,12 @@ class SimpleStatisticsManager(InterfaceStatistics):
         self.db_name = database_name
 
     def save_statistics(self, statistics):
-        #db = TinyDB('{}'.format(self.db_name))
-        #db.insert({'flow_id': statistics[0], 'size': statistics[1], 'time':
-        #           statistics[2]})
-        #db.close()
-        return True
+        db = TinyDB('{}'.format(self.db_name))
+        db.insert({'flow_id': statistics[0], 'size': statistics[1], 'time':
+                   statistics[2]})
+        db.close()
 
     def get_statistics(self, flow_id, max_stat, time_from_str, time_to_str):
-        return []
-        """
         db = TinyDB('{}'.format(self.db_name))
         query = db.search(Query().flow_id == flow_id)
         db.close()
@@ -38,4 +35,4 @@ class SimpleStatisticsManager(InterfaceStatistics):
             return result[size-max_stat:]
         else:
             return result
-        """
+s
